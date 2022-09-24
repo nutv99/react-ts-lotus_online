df490f7f8ae0004e58e9e4de7832c2870f60af9f
nutv99 <nutv99@gmail.com>
2022-09-24 14:31:50 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="header99">
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <div className="topbar flex">
+        <div>ดาวน์โหลด</div>
+        <div>ข้อมูลข่าวสาร</div>
+      </div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-lotus_online
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-qjny3y)
