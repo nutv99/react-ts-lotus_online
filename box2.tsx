65d64fc87f01fd9438a873891c37fd88f4214706
nutv99 <nutv99@gmail.com>
2022-09-27 15:36:09 +0700

    1 file modified

--- box2.tsx
+++ box2.tsx
@@ -1,4 +1,38 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import ApiService from './Apiservice.tsx';
+import ApiService from './Apiservice';
+
+
+const testApi = async () => {
+  // Test Get DATA
+  try {
+    
+    // const usersData = await ApiService.httpGet(
+    //   'dataservice/clsItemMaster.php'
+    // );
+    const usersData = await ApiService.axiosGet('dataservice/clsItemMaster.php') ;
+    // const usersData = '{}';
+    // axios
+    // .get("https://lovetoshopmall.com/dataservice/clsItemMaster.php")
+    // .then(data => console.log(data.data))
+    // .catch(error => console.log(error));
+
+    
+
+    
+    console.log('userData', usersData);
+    //setUsers(usersData);
+    
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    
+  }
+}; 
+
+
+
+useEffect(() => {
+  testApi();
+}, []);
