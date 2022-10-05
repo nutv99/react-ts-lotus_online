61844a42642108a1686e4d7327bfd658e3edefb2
nutv99 <nutv99@gmail.com>
2022-10-05 14:22:41 +0700

    1 file modified

--- pages/Cart.tsx
+++ pages/Cart.tsx
@@ -8,22 +8,27 @@ import NewUtil from '../service/newutil';
 import { BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  
-   
+
+import  winStorage  from '../service/winStorage' ; 
+
+// import winStorage from '../service/winStorage';   
  // Section 2 **************** Start Declare Var  ********
 const [item2s, setItem2s] = useState([]);
 const [loading, setLoading] = useState(false);
 const count = 0;
     
  //***********************   Start Declare Function  **********************   
-const getDataAPI = async () => {
+const getDataAPI = async () => { 
+
+//let numOrder = winStorage.getNumOrderOnLocal('guest',thisItemCode,0) ; 
 try {
      setLoading(true);
       let endPoint = 'dataservice/clsItemMaster.php';
       const usersData = await ApiService.axiosGet(endPoint);
       setItem2s(item2s);
       console.log('user from main', usersData);
-      setLoading(false);
+      setLoading(false); 
+      
  } catch (err) {
       console.error(err.message);
  } finally {
